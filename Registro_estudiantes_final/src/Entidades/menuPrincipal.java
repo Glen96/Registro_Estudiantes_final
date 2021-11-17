7afb45713fd33f7bd0c594140cddec56eebc74ff
AndoreiCR <dexter_andrey@hotmail.com>
2021-11-17 15:07:56 -0600

    Revision de codigo
    Se actualiza el codigo de Estudiante y se crean los menus necesarios

--- Registro_estudiantes_final/src/Entidades/menuPrincipal.java
+++ Registro_estudiantes_final/src/Entidades/menuPrincipal.java
@@ -5,10 +5,11 @@ import javax.swing.JOptionPane;
 public class menuPrincipal {
 
     public Universidad universidad1 = new Universidad();
+    int opc;
 
     // metodo para trabajar el menu principal
     public void llamarMenu() {
-        int opc = Integer
+        opc = Integer
                 .parseInt(JOptionPane.showInputDialog(null, "!Bienvenido!\n" + "Por favor elija la opcion que desea\n"
                         + "1. Modulo Estudiantes\n" + "2. Consultas a la Universidad\n" + "0. Salir\n"));
         switch (opc) {
@@ -32,6 +33,38 @@ public class menuPrincipal {
             }
             break;
         default:
+            break;
+        }
+    }
+
+    public void llamarMenuPrincipal() {
+        opc = Integer.parseInt(JOptionPane.showInputDialog(null,
+                "!Bienvenido a la Universidad los Patitos!\n" + "Por favor elija la opcion que desea\n"
+                        + "1. Modulo Estudiantes\n" + "2. Consultas a la Universidad\n" + "3. Consultas a Profesores\n"
+                        + "4. Salir"));
+
+        switch (opc) {
+        default:
+            JOptionPane.showMessageDialog(null, "Opcion ingresada es incorrecta", "Opcion Invalida",
+                    JOptionPane.ERROR_MESSAGE);
+            llamarMenuPrincipal();
+            break;
+        case 1:
+            Estudiante estudiante = new Estudiante();
+            estudiante.LlamarMenuEstudiante();
+            break;
+        case 2:
+            Universidad universidad = new Universidad();
+            universidad.LlamarMenuUniversidad();
+            break;
+        case 3:
+            Profesor profesor = new Profesor();
+            profesor.LlamarMenuProfesor();
+            break;
+        case 4:
+            JOptionPane.showMessageDialog(null, "Gracias por utilizar nuestro sistema", "Saliendo",
+                    JOptionPane.INFORMATION_MESSAGE);
+            System.exit(0);
             break;
         }
     }
